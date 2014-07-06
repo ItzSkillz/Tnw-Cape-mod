c781b509fb56b565feb5bba5112b45f8eb641024
Navarr Barnier <me@navarr.me>
2014-07-05 22:50:49 -0400

    Upped the professionalism

--- src/main/java/com/mineznightswatch/cape/Tnw_Cape_Mod.java
+++ src/main/java/com/mineznightswatch/cape/Tnw_Cape_Mod.java
@@ -1,15 +1,4 @@
-package com.github.ItzSkillz;
-
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.embedded.EmbeddedChannel;
-
-import java.awt.image.BufferedImage;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.util.*;
-import java.util.logging.Handler;
-import java.lang.reflect.Field;
-import java.net.*;
+package com.mineznightswatch.cape;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
@@ -18,24 +7,27 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.Phase;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.multiplayer.WorldClient;
-import net.minecraft.client.renderer.IImageBuffer;
-import net.minecraft.client.renderer.ImageBufferDownload;
 import net.minecraft.client.renderer.ThreadDownloadImageData;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.ITextureObject;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.src.*;
 import net.minecraft.util.ResourceLocation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Mod(modid = Tnw_Cape_Mod.MODID, version = Tnw_Cape_Mod.VERSION)
 public class Tnw_Cape_Mod {
     public static final String MODID = "TNW_cape_mod";
-    public static final String VERSION = "0.5-Beta";
+    public static final String VERSION = "1.0.0";
 
     int tick = 0;
     int sweep = 20;
@@ -43,21 +35,16 @@ public class Tnw_Cape_Mod {
     private ArrayList<String> capeDIRs = null;
 
     private HashMap<String, ThreadDownloadImageData> checked = new HashMap<String, ThreadDownloadImageData>();
-    private ArrayList<String> applied = new ArrayList<String>();
     private ArrayList<String> ignored = new ArrayList<String>();
     private ArrayList<String> players = new ArrayList<String>();
 
-
-    private String stdCapesDir    = "http://minez-nightswatch.com/mod/cape?user=";
-    private String hdCapesDir    = "http://minez-nightswatch.com/mod/cape?user=";
+    private String capesDir = "http://minez-nightswatch.com/mod/cape?user=";
 
     boolean checking = false;
     boolean shouldClear = false;
-    int _iCanHazHDMod = -1;
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         findCapesDirectories();
 
         FMLCommonHandler.instance().bus().register(this);
@@ -68,14 +55,6 @@ public class Tnw_Cape_Mod {
         if (event.phase == Phase.END) {
             updateCloakURLs();
         }
-    }
-
-    //////////////////////////////////////////
-    ///// private stuff
-    //////////////////////////////////////////
-
-    private void checkForUpdate() {
-        // To be removed.
     }
 
 
@@ -93,7 +72,7 @@ public class Tnw_Cape_Mod {
 
                 ArrayList<String> _capeDIRs = new ArrayList<String>();
                 try {
-                    URL dirList = new URL("http://www.minecapes.net/capesDirectory.list");
+                    URL dirList = new URL("http://minez-nightswatch.com/capesDirectory.list");
                     BufferedReader in = new BufferedReader(new InputStreamReader(dirList.openStream()));
 
                     String inputLine;
@@ -104,39 +83,13 @@ public class Tnw_Cape_Mod {
                     System.out.println("[TNW] External cape directories could not be found. Try again on restart...");
                 }
 
-                // add default dir
-                _capeDIRs.add(0, stdCapesDir);
-
-                if (iCanHazHDMod()) {
-                    System.out.println("[TNW] Found HD Patch! Adding HD directory.");
-                    _capeDIRs.add(0, hdCapesDir);
-                }
+                _capeDIRs.add(0, capesDir);
 
                 System.out.println("[TNW] " + _capeDIRs.size() + " directories loaded!");
                 capeDIRs = _capeDIRs;
             }
 
         }.start();
-
-    }
-    /*
-    private void handleMCMessage(String message) {
-        System.out.println("[TNW] Got a message: " + message);
-        if (message.equalsIgnoreCase("reloadCapes")) {
-            shouldClear = true;
-        }
-    }
-    */
-
-    private boolean iCanHazHDMod() {
-        if (_iCanHazHDMod == -1) {
-            if (new ImageBufferDownload().parseUserSkin(new BufferedImage(128,64,BufferedImage.TYPE_INT_RGB)).getWidth() == 64) {
-                _iCanHazHDMod = 0;
-            } else {
-                _iCanHazHDMod = 1;
-            }
-        }
-        return (_iCanHazHDMod == 1 ? true : false);
 
     }
 
@@ -243,19 +196,20 @@ public class Tnw_Cape_Mod {
 
                 String url = capeURLcheck + removeColorFromString(playerName) + ".png";
                 try {
-                    HttpURLConnection con =    (HttpURLConnection) new URL(url).openConnection();
+                    HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
                     con.setRequestMethod("HEAD");
-                    con.setRequestProperty ( "User-agent", "MineCapes " + VERSION);
-                    con.setRequestProperty ( "Java-Version", System.getProperty("java.version"));
+                    con.setRequestProperty("User-agent", "MineCapes " + VERSION);
+                    con.setRequestProperty("Java-Version", System.getProperty("java.version"));
                     con.setConnectTimeout(2000);
                     con.setUseCaches(false);
-                    con.setFollowRedirects(false);
 
                     if (con.getResponseCode() == HttpURLConnection.HTTP_OK) found = url;
 
                     con.disconnect();
 
-                } catch (Exception e) {}
+                } catch (Exception e) {
+                    // Expected Failure if no cape
+                }
 
 
                 if (found != null) break;
@@ -269,11 +223,11 @@ public class Tnw_Cape_Mod {
                 AbstractClientPlayer aPlayer = (AbstractClientPlayer) Minecraft.getMinecraft().theWorld.getPlayerEntityByName(playerName);
 
                 // get cloak
-                ResourceLocation resourcePackCloak = aPlayer.getLocationCape(aPlayer.getCommandSenderName());
+                ResourceLocation resourcePackCloak = AbstractClientPlayer.getLocationCape(aPlayer.getCommandSenderName());
 
                 TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
                 ThreadDownloadImageData object = new ThreadDownloadImageData(found, null, null);
-                texturemanager.loadTexture(resourcePackCloak, (ITextureObject)object);
+                texturemanager.loadTexture(resourcePackCloak, (ITextureObject) object);
 
                 checked.put(playerName, object);
                 System.out.println("[TNW] Found cloak: " + found);
