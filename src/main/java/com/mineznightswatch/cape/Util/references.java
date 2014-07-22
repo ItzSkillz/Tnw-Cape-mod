e479fee62ed3ae9268a9dbc394bf8db080089020
ItzSkillz <falling.dutchman@gmail.com>
2014-07-22 21:23:52 +0200

    up'ed the version

--- src/main/java/com/mineznightswatch/cape/Util/references.java
+++ src/main/java/com/mineznightswatch/cape/Util/references.java
@@ -3,7 +3,7 @@ package com.mineznightswatch.cape.Util;
 public abstract class references
 {
     public static final String MOD_ID = "TNW_cape_mod";
-    public static final String MOD_VERSION = "1.0.0";
+    public static final String MOD_VERSION = "1.1.0";
     public static final String MOD_NAME = "TNW_Cape";
     public static final String CAPES_DIR = "http://minez-nightswatch.com/mod/cape?user=";
 }
